0fb8e339323e0545215e7afd2a88be784e2c792c
Fatimata <fatimata.mbaye@hotmail.com>
2018-07-25 17:53:44 +0200

    creation classe métier AuthenticationClientLoginViewModel creation du controller d'authentification des clients

---
+++ BoVoyageProjetFinal/Controllers/AuthenticationClientController.cs
@@ -0,0 +1,57 @@
+﻿using BoVoyageProjetFinal.Data;
+using BoVoyageProjetFinal.Models;
+using BoVoyageProjetFinal.Views.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageProjetFinal.Controllers
+{
+    public class AuthenticationClientController : Controller
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+        // GET: BackOffice/Authentication/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Login(AuthenticationClientLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var passwordHash = model.Password.HashMD5();
+                var client = db.Clients.SingleOrDefault(x => x.Mail == model.Login && x.Password == passwordHash);
+                if (client == null)
+                {
+                    //1
+                    //ModelState.AddModelError("", "Utilisateur ou mot de passe incorrect.");
+
+                    //2
+                    ViewBag.ErrorMessage = "Utilisateur ou mot de passe incorrect.";
+                    return View(model);
+                }
+                else
+                {
+                    Session.Add("CLIENT_BO", client);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+
+        }
+
+
+        protected override void Dispose(bool disposing) //pour liberer connexion à la base de donnees lorque controleur a fini de l'utiliser
+        {
+           if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BoVoyageProjetFinal/Models/AuthenticationClientLoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjetFinal.Models
+{
+    public class AuthenticationClientLoginViewModel
+    {
+        [Display(Name = "Adresse Email")]
+        [Required(ErrorMessage = "le champ {0} est obligatoire")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                   @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                   @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                   ErrorMessage = "L'adresse mail n'est pas au bon format")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
