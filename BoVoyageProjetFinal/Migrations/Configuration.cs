bb55d7bcd735338b6ec562d2be362f9140562d27
nabil bellil <abilaz24@gmail.com>
2018-07-25 15:38:47 +0200

    Création du dossier migration

---
+++ BoVoyageProjetFinal/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjetFinal.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<BoVoyageDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BoVoyageDbContext context)
+        {
+            context.Civilities.AddOrUpdate(
+                new Models.Civility { Label = "Monsieur" },
+                new Models.Civility { Label = "Madame" },
+                new Models.Civility { Label = "Mademoiselle" });
+        }
+    }
+}
