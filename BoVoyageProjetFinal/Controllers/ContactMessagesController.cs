9f85606a928ee335f161d6c38fb2af84f05dec88
nabil bellil <abilaz24@gmail.com>
2018-07-25 17:34:53 +0200

    Création de ContactMessagesController et de Views\ContactMessages

---
+++ BoVoyageProjetFinal/Controllers/ContactMessagesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageProjetFinal.Data;
+using BoVoyageProjetFinal.Models;
+
+namespace BoVoyageProjetFinal.Controllers
+{
+    public class ContactMessagesController : Controller
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: ContactMessages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactMessages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Lastname,Firstname,Email,Message,CreatedAt,Deleted,DeletedAt")] ContactMessage contactMessage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactMessages.Add(contactMessage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactMessage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
