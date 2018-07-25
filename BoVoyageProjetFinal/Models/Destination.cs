34a9a978fa8291e639aef3d75673f1368140bb3a
Cyl-Dev <Cyl-Dev@outlook.fr>
2018-07-25 13:05:19 +0200

    Création des classes "BaseModel", "Travel", "TravelAgency", "Destination"

---
+++ BoVoyageProjetFinal/Models/Destination.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageProjetFinal.Models
+{
+    [Table(name: "Destinations")]
+    public class Destination : BaseModel
+    {
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [StringLength(15)]
+        [Display(Name = "Continent")]
+        public string Continent { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [StringLength(30)]
+        [Display(Name = "Pays")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [StringLength(30)]
+        [Display(Name = "Région")]
+        public string Region { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Description")]
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string Description { get; set; }
+
+        public ICollection<Travel> Travels { get; set; }
+    }
+}
---
+++ BoVoyageProjetFinal/Models/TravelAgency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjetFinal.Models
+{
+    [Table(name: "TravelAgencies")]
+    public class TravelAgency : BaseModel
+    {
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [StringLength(30)]
+        [Display(Name = "Agence de voyage")]
+        public string Name { get; set; }
+    }
+}
