6d8f33308e3b4a3ce671164ab4a595d43d605d74
Fatimata <fatimata.mbaye@hotmail.com>
2018-07-25 16:17:22 +0200

    ajout validateur mail et majorité

--- BoVoyageProjetFinal/Utils/Validator/ExistingMailUser.cs
+++ BoVoyageProjetFinal/Utils/Validator/ExistingMailUser.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using BoVoyageProjetFinal.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageProjetFinal.Utils.Validator
 {
-    public class ExistingMailUser
+    public class ExistingMailUser : ValidationAttribute
     {
+        public override bool IsValid(object value)
+        {
+            using (BoVoyageDbContext db = new BoVoyageDbContext())
+            {
+                return !db.Clients.Any(x => x.Mail == value.ToString());
+            }
+        }
     }
 }
---
+++ BoVoyageProjetFinal/Utils/Validator/Major.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjetFinal.Utils.Validator
+{
+    public class Major : ValidationAttribute
+    {
+        private int years;
+        public Major(int years)
+        {
+            this.years = years;
+        }
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime)
+            {
+                var dt = (DateTime)value; // je met mon objet en datetime
+                return dt.AddYears(18) <= DateTime.Now; //test majorité
+            }
+            return false;
+        }
+    }
+}
