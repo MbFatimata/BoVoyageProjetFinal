20a8f235c261df3dffbd2169e16c2423d4c053ed
Jennifer <jennifer_martin@outlook.fr>
2018-07-25 09:10:52 +0200

    Ajoutez des fichiers projet.

---
+++ BoVoyageProjetFinal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoVoyageProjetFinal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
