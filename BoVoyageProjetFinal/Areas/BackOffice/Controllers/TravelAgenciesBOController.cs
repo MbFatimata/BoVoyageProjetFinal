ff2f525075d794b5d5557e0c0f09176d4f2dbb63
nabil bellil <abilaz24@gmail.com>
2018-07-26 15:11:02 +0200

    Corrections sur les vues dans le BackOffice

---
+++ BoVoyageProjetFinal/Areas/BackOffice/Controllers/TravelAgenciesBOController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageProjetFinal.Data;
+using BoVoyageProjetFinal.Models;
+
+namespace BoVoyageProjetFinal.Areas.BackOffice.Controllers
+{
+    public class TravelAgenciesBOController : Controller
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: BackOffice/TravelAgenciesBO
+        public ActionResult Index()
+        {
+            return View(db.TravelAgencies.ToList());
+        }
+
+        // GET: BackOffice/TravelAgenciesBO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TravelAgency travelAgency = db.TravelAgencies.Find(id);
+            if (travelAgency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travelAgency);
+        }
+
+        // GET: BackOffice/TravelAgenciesBO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackOffice/TravelAgenciesBO/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,CreatedAt,Deleted,DeletedAt")] TravelAgency travelAgency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TravelAgencies.Add(travelAgency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(travelAgency);
+        }
+
+        // GET: BackOffice/TravelAgenciesBO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TravelAgency travelAgency = db.TravelAgencies.Find(id);
+            if (travelAgency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travelAgency);
+        }
+
+        // POST: BackOffice/TravelAgenciesBO/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,CreatedAt,Deleted,DeletedAt")] TravelAgency travelAgency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(travelAgency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(travelAgency);
+        }
+
+        // GET: BackOffice/TravelAgenciesBO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TravelAgency travelAgency = db.TravelAgencies.Find(id);
+            if (travelAgency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travelAgency);
+        }
+
+        // POST: BackOffice/TravelAgenciesBO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TravelAgency travelAgency = db.TravelAgencies.Find(id);
+            db.TravelAgencies.Remove(travelAgency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
