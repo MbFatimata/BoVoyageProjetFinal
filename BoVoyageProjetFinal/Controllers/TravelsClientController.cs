32d66b47558180ca5a04646620b3ff10c62408ce
Fatimata <fatimata.mbaye@hotmail.com>
2018-07-27 09:54:04 +0200

    creation controller et vue des voyages clients

---
+++ BoVoyageProjetFinal/Controllers/TravelsClientController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageProjetFinal.Data;
+using BoVoyageProjetFinal.Models;
+
+namespace BoVoyageProjetFinal.Controllers
+{
+    public class TravelsClientController : Controller
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: TravelsClient
+        public ActionResult Index()
+        {
+            var travels = db.Travels.Include(t => t.Destination).Include(t => t.TravelAgency);
+            return View(travels.ToList());
+        }
+
+        // GET: TravelsClient/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Travel travel = db.Travels.Find(id);
+            if (travel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
